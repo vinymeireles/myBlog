750d9a33118d92be43432a8d9f00c921f0732880
Viny <viniciusmeireles@gmail.com>
2022-02-11 19:16:07 -0300

    Projeto inicial Web Blog de publicações em Python - Django

---
+++ blog/models.py
@@ -0,0 +1,39 @@
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')    
+
+#classe postagem com os campos do banco
+class Post(models.Model):
+   
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250) 
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publish')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+
+#classe meta para ordenar as postagens decrescente(mais recentes)
+class Meta:
+    ordering = ('-publish',)
+
+
+
+#método padrão do django
+def __str__(self):
+    return self.title
+
